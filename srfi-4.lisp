ba8ed1b5c7f0a142963d6080b1e714589f8ad24d
g000001 <chiba.masaomi@gmail.com>
2012-01-10 07:47:02 +0900

    add: some optimize description.

--- srfi-4.lisp
+++ srfi-4.lisp
@@ -2,70 +2,90 @@
 
 (cl:in-package :srfi-4-internal)
 
-(dolist (s '(s u f))
-  (dolist (n '(8 16 32 64))
-    (let ((pre (format nil "~A~D" s n)))
-      (when (or (string/= 'f s)
-                (and (string= 'f s)
-                     (or (= 32 n)
-                         (= 64 n))))
-        (let ((type-spec (case s
-                           ((s) `(signed-byte ,n))
-                           ((u) `(unsigned-byte ,n))
-                           ((f) (if (= 32 n)
-                                    'single-float
-                                    'double-float)))))
-          ;; TAGvector?
-          (let ((name (intern (format nil "~AVECTOR?" pre)))
-                (type (intern (format nil "~AVEC" pre))))
-            (eval `(defun ,name (,type)
-                     (typep ,type '(vector ,type-spec)))))
-          ;; make-TAGvector
-          (let ((name (intern (format nil "MAKE-~AVECTOR" pre))))
-            (eval `(defun ,name (size &optional (fill 0))
-                     (make-array size
-                                 :initial-element (coerce fill ',type-spec)
-                                 :element-type ',type-spec
-                                 :adjustable nil))))
-          ;; TAGvector
-          (let ((name (intern (format nil "~AVECTOR" pre)))
-                (elts (intern (format nil "~AS" pre))))
-            (eval `(defun ,name (&rest ,elts)
-                     (make-array (length ,elts)
-                                 :initial-contents ,elts
-                                 :element-type ',type-spec
-                                 :adjustable nil))))
-          ;; TAGvector-length
-          (let ((name (intern (format nil "~AVECTOR-LENGTH" pre)))
-                (vec (intern (format nil "~AVEC" pre))))
-            (eval `(defun ,name (,vec)
-                     (declare ((vector ,type-spec) ,vec))
-                     (length ,vec))))
-          ;; TAGvector-ref
-          (let ((name (intern (format nil "~AVECTOR-REF" pre)))
-                (vec (intern (format nil "~AVEC" pre))))
-            (eval `(defun ,name (,vec i)
-                     (declare ((vector ,type-spec) ,vec)
-                              (unsigned-byte i))
-                     (aref ,vec i))))
-          ;; TAGvector-set!
-          (let ((name (intern (format nil "~AVECTOR-SET!" pre)))
-                (vec (intern (format nil "~AVEC" pre))))
-            (eval `(defun ,name (,vec i value)
-                     (declare ((vector ,type-spec) ,vec)
-                              (,type-spec value)
-                              (unsigned-byte i))
-                     (setf (aref ,vec i) value))))
-          ;; TAGvector->list
-          (let ((name (intern (format nil "~AVECTOR->LIST" pre)))
-                (vec (intern (format nil "~AVEC" pre))))
-            (eval `(defun ,name (,vec)
-                     (declare ((vector ,type-spec) ,vec))
-                     (coerce ,vec 'list))))
-          ;; list->TAGvector
-          (let ((name (intern (format nil "LIST->~AVECTOR" pre))))
-            (eval `(defun ,name (list)
-                     (declare (list list))
-                     (coerce list '(simple-array ,type-spec (*)))))))))))
+(defmacro coll (&body body)
+  (let ((tem (gensym "TEM-"))
+        (ans (gensym "ANS-")))
+    `(macrolet ((yield (&body body)
+                  `(setq ,',tem (cdr (rplacd ,',tem (list (progn ,@body)))))))
+       (let* ((,ans (list nil))
+              (,tem ,ans))
+         ,@body
+         (cdr ,ans)))))
+
+(defmacro defun-inline (name (&rest args) &body body)
+  `(progn
+     (declaim (inline ,name))
+     (defun ,name (,@args) ,@body) ))
+
+(defmacro defsrfi-4 ()
+  `(progn
+     ,@(coll
+         (dolist (s '(s u f))
+           (dolist (n '(8 16 32 64))
+             (let ((pre (format nil "~A~D" s n)))
+               (when (or (string/= 'f s)
+                         (and (string= 'f s)
+                              (or (= 32 n)
+                                  (= 64 n) )))
+                 (let ((type-spec (case s
+                                    ((s) `(signed-byte ,n))
+                                    ((u) `(unsigned-byte ,n))
+                                    ((f) (if (= 32 n)
+                                             'single-float
+                                             'double-float )))))
+                   ;; TAGvector?
+                   (let ((name (intern (format nil "~AVECTOR?" pre)))
+                         (type (intern (format nil "~AVEC" pre))) )
+                     (yield `(defun-inline ,name (,type)
+                               (typep ,type '(vector ,type-spec)) )))
+                   ;; make-TAGvector
+                   (let ((name (intern (format nil "MAKE-~AVECTOR" pre))))
+                     (yield `(defun-inline ,name (size &optional (fill 0))
+                               (make-array size
+                                           :initial-element (coerce fill ',type-spec)
+                                           :element-type ',type-spec
+                                           :adjustable nil))))
+                   ;; TAGvector
+                   (let ((name (intern (format nil "~AVECTOR" pre)))
+                         (elts (intern (format nil "~AS" pre))) )
+                     (yield `(defun-inline ,name (&rest ,elts)
+                               (make-array (length ,elts)
+                                           :initial-contents ,elts
+                                           :element-type ',type-spec
+                                           :adjustable nil))))
+                   ;; TAGvector-length
+                   (let ((name (intern (format nil "~AVECTOR-LENGTH" pre)))
+                         (vec (intern (format nil "~AVEC" pre))) )
+                     (yield `(defun-inline ,name (,vec)
+                               (declare ((vector ,type-spec) ,vec))
+                               (length ,vec) )))
+                   ;; TAGvector-ref
+                   (let ((name (intern (format nil "~AVECTOR-REF" pre)))
+                         (vec (intern (format nil "~AVEC" pre))) )
+                     (yield `(defun-inline ,name (,vec i)
+                               (declare ((simple-array ,type-spec (*)) ,vec)
+                                        (unsigned-byte i) )
+                               (aref ,vec i) )))
+                   ;; TAGvector-set!
+                   (let ((name (intern (format nil "~AVECTOR-SET!" pre)))
+                         (vec (intern (format nil "~AVEC" pre))) )
+                     (yield `(defun-inline ,name (,vec i value)
+                               (declare ((vector ,type-spec) ,vec)
+                                        (,type-spec value)
+                                        (unsigned-byte i) )
+                               (setf (aref ,vec i) value) )))
+                   ;; TAGvector->list
+                   (let ((name (intern (format nil "~AVECTOR->LIST" pre)))
+                         (vec (intern (format nil "~AVEC" pre))) )
+                     (yield `(defun-inline ,name (,vec)
+                               (declare ((vector ,type-spec) ,vec))
+                               (coerce ,vec 'list) )))
+                   ;; list->TAGvector
+                   (let ((name (intern (format nil "LIST->~AVECTOR" pre))))
+                     (yield `(defun-inline ,name (list)
+                               (declare (list list))
+                               (coerce list '(simple-array ,type-spec (*))) )))))))))))
+
+(defsrfi-4)
 
 ;;; eof
